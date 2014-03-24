7472bcacb0c3a0ed3fd011eaea3e646b8c15ca39
Greg <gregmichaeldavies@gmail.com>
2014-03-24 17:19:28 +0000

    Included selection area for tracks

--- code/module/sequencer/directive/track-list.js
+++ code/module/sequencer/directive/track-list.js
@@ -20,6 +20,33 @@ qh.getModule('sequencer').directive('trackList', function() {
 				// Chosen bar is the time chosen by the click in bars.
 				// This will be the starting position for the pattern. The pattern length will be a default if the off-click has already happened.
 			};
+			$scope.select = {
+				trackId: 0,
+				setStart: function(trackId, $event) {
+					var screenPos = $event.offsetX/$event.currentTarget.offsetWidth;
+					var chosenBar = (screenPos*bar.view.zoom)+bar.view.offset;
+
+					$scope.select.trackId = trackId;
+					$scope.select.start = $event.offsetX;
+					$scope.select.changing = true;
+				},
+				setChange: function($event) {
+					if ($scope.select.changing) {
+						$scope.select.end = $event.offsetX;
+						$scope.select.width = $scope.select.end-$scope.select.start;
+						console.log($event.offsetX, $event.offsetY, $scope.select.start, $scope.select.width, $scope.select.end);
+					}
+				},
+				setEnd: function() {
+					$scope.select.on = true;
+					$scope.select.changing = false;
+				},
+				start: 0,
+				width: 0,
+				end: 0,
+				changing: false,
+				on: false,
+			};
 		}],
 	};
 });
